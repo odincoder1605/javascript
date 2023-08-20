4eafc3742ac2b0823d44f432dc009b8432b1bb94
Himadri Roy <141954844+odincoder1605@users.noreply.github.com>
2023-08-20 07:00:45 +0000

    data types done

---
+++ 01_basics/data_types.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as never version
+
+let name ="Himadri"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.table([age,name,isLoggedIn])
